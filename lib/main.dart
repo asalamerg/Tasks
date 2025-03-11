38e1eb3bb3cb89a5dff05d7978abe3337f9fbbfc
ahmad <asalameh02gmail.comm>
2025-03-11 03:19:19 +0200

    integrate with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:tasks/core/theme/app_theme.dart';
 import 'package:tasks/feature/home/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp( Tasks());
 }
 
